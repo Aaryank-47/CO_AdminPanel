ae185d781da1439635a948ac73725b44554d6ce8
aaryan <aaryankamalwanshi274@gmail.com>
2025-06-12 15:12:43 +0530

    finally solved the error of fixing size of admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+
 import Dashboard from "./pages/Dashboard.jsx";
 import Food from "./pages/Food.jsx";
 import Order from "./pages/Order.jsx";
@@ -11,28 +14,27 @@ import Settings from "./pages/Settings.jsx";
 import Notification from "./pages/Notification.jsx";
 import AdminProfile from "./pages/AdminProfile.jsx";
 import Login from "./pages/Login.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
 
 
 const App = () => {
   return (
     
       <>
-        <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+        <div className="w-screen flex h-screen v-screen bg-gray-50 dark:bg-gray-900">
           {/* Sidebar - Only visible when authenticated */}
           <PrivateRoute>
             <Sidebar />
           </PrivateRoute>
 
           {/* Main Area */}
-          <div className="flex flex-col flex-1 overflow-hidden">
+          <div className="w-full flex flex-col flex-1 overflow-hidden overflow-x-auto">
             {/* Navbar - Only visible when authenticated */}
             <PrivateRoute>
               <Navbar />
             </PrivateRoute>
 
             {/* Page Content */}
-            <main className="flex-1 overflow-y-auto p-4 md:p-6">
+            <main className="w-full flex-1 overflow-y-auto p-4 md:p-6">
               <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route
